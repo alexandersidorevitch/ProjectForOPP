2bf6d5f1f0919dc95cbd3f33d724e350d969a073
kirsanovtaras8@gmail.com <kirsanovtaras8@gmail.com>
2021-02-25 13:44:25 +0300

    Добавлены функции CRUD в User

--- ProjectForOOP/User.cpp
+++ ProjectForOOP/User.cpp
@@ -1,5 +1,7 @@
 #include "User.h"
 #include <iostream>
+#include <string>
+#include <iomanip>
 using namespace std;
 
 User::User()
@@ -20,6 +22,68 @@ string User::GetLogin() const
 string User::GetPassword() const
 {
 	return this->Password;
+}
+
+void User::Input()
+{
+	cout << "Введите Логин" << endl;
+	getline(cin, Login);
+	cout << "Введите Пароль" << endl;
+	getline(cin, Password);
+}
+
+void User::Append(vector<User>& users, User user)
+{
+	users.push_back(user);
+}
+
+void User::Delete(vector<User>& users, int index)
+{
+	if (index < 0 || index >= users.size())
+	{
+		cout << "Такого порядкого номера нет" << endl;
+	}
+	else {
+		users.erase(users.begin() + index);
+		cout << "Запись удалена" << endl;
+	}
+}
+
+void User::Edit(vector<User>& users, int index)
+{
+	if (index < 0 || index >= users.size())
+	{
+		cout << "Такого порядкого номера нет" << endl;
+	}
+	else {
+		users[index].Input();
+	}
+}
+
+void User::PrintAsTable(vector<User>& users)
+{
+	PrintAsTable(users, '|', '_');
+}
+
+void User::PrintAsTable(vector<User>& users, char verticalDelemitr, char gorizontalDelemitr)
+{
+	setlocale(0, "");
+	int maxLenghtLogin = 5, maxLenghtPassword = 6;
+	for (auto user : users) {
+		maxLenghtLogin = __max(user.Login.length(), maxLenghtLogin);
+		maxLenghtPassword = __max(user.Password.length(), maxLenghtPassword);
+	}
+	int totalLenght = maxLenghtLogin + maxLenghtPassword ;
+	string line(totalLenght + 3, gorizontalDelemitr);
+	cout << line << endl;
+	cout << verticalDelemitr << setw(maxLenghtLogin) << "Логин" << verticalDelemitr
+		<< setw(maxLenghtPassword) << "Пароль" << verticalDelemitr;
+	cout << line << endl;
+	for (auto user : users) {
+		cout << verticalDelemitr << setw(maxLenghtLogin) << user.Login << verticalDelemitr
+			<< setw(maxLenghtPassword) << user.Password << verticalDelemitr;
+		cout << line << endl;
+	}
 }
 
 
--- ProjectForOOP/User.h
+++ ProjectForOOP/User.h
@@ -1,5 +1,6 @@
 
 #include <string>
+#include <vector>
 using namespace std;
 
 class User
@@ -10,6 +11,12 @@ public:
 
 	string GetLogin() const;
 	string GetPassword() const;
+	void Input();
+	static void Append(vector<User>& users, User user);
+	static void Delete(vector<User>& users, int index);
+	static void Edit(vector<User>& users, int index);
+	static void PrintAsTable(vector<User>& members);
+	static void PrintAsTable(vector<User>& members, char verticalDelemitr, char gorizontalDelemitr);
 
 	friend ostream& operator<<(ostream& out, const User& user);
 	friend istream& operator>>(istream& in, User& user);
